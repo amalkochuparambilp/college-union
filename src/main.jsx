6faca97381650cf67733535201c916a8556256db
amalkochuparambilp <98939651+amalkochuparambilp@users.noreply.github.com>
2024-11-05 20:52:48 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Tapp from './Tapp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Tapp />
   </StrictMode>,
 )
