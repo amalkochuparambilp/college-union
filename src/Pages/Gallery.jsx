15274ff884bff63fde99b901fcbf359b180ce780
amalkochuparambilp <98939651+amalkochuparambilp@users.noreply.github.com>
2024-11-05 20:41:02 +0530

    Update and rename Gallery.css to Gallery.jsx

---
+++ src/Pages/Gallery.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../Components/Navbar/Navbar.css'
+
+const Gallery = () => {
+return(
+  <div style={{height: '100vh',display: 'flex',justifyContent: 'center',alignItems: 'center'}}>
+    <h6>Under Development</h6>
+  </div>
+)
+}
