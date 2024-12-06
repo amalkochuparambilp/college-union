9282946a5be4fff8e13f842ce434ae364671f944
amalkochuparambilp <98939651+amalkochuparambilp@users.noreply.github.com>
2024-12-06 09:59:03 +0530

    Update ProgramUpdate.jsx

--- src/Pages/ProgramUpdate.jsx
+++ src/Pages/ProgramUpdate.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 const ProgramUpdate = () => {
     return(
-    <div style={{height: '100vh',display: 'flex',justifyContent: 'center',alignItems: 'center'}}>
-        <h6>Under Development</h6>
+            <div className="about-right">
+            <h3>ABOUT JNIAS UC</h3>
+            <h2>Mission</h2>
+            <p>Our mission is to foster a dynamic learning community that empowers individuals to excel academically, embrace diversity, and contribute positively to society. Grounded in the principles of Democracy, integrity, innovation, and inclusivity, we are committed to providing a holistic education that nurtures intellectual curiosity, cultivates critical thinking skills, and fosters a passion for lifelong learning and to prepare ourstudents to meet the challenges of a rapidly changing world with confidence and competence. Our goal is to inspire excellence, creativity, and ethical leadership, empowering our graduates to make meaningful contributions in their chosen fields and become responsible global citizens.</p>
+        </div>
     </div>
     )
 }
