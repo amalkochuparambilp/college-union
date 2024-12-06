aac89eaab0989ea7dd2ea81f82cc431b2526dab3
amalkochuparambilp <98939651+amalkochuparambilp@users.noreply.github.com>
2024-12-06 13:58:52 +0530

    Update Collageuc.jsx

--- src/Collageuc.jsx
+++ src/Collageuc.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { HashRouter, Route, Routes, Link } from "react-router-dom";
 import App from "./App";
 import Dev from "./Pages/dev";
+import ProgramUpdate from "./Pages/ProgramUpdate";
 
 const Collagecu = () => {
     return (
@@ -9,7 +10,7 @@ const Collagecu = () => {
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="akpdevz" element={<Dev />} />
-                <Route path="unionprogram" element={<ProgamUpdate />} />
+                <Route path="unionprogram" element={<ProgramUpdate />} />
             </Routes>
         </HashRouter>
     );
