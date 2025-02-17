e05253fc9925ee925f776629c61ae2c55f00ca1a
amalkochuparambilp <98939651+amalkochuparambilp@users.noreply.github.com>
2025-02-17 14:51:02 +0530

    Update Ambiora.jsx

--- src/Pages/Ambiora.jsx
+++ src/Pages/Ambiora.jsx
@@ -1,1 +1,16 @@
-yu
+import React from "react";
+import './Page.css'
+
+const Ambiora = () => {
+    return(
+<div className="about">
+            <div className="about-right">
+            <h3>Ambiora</h3>
+            <h2>Arts Day of JNIAS</h2>
+            <p>Update Soon..</p>
+        </div>
+</div>
+    )
+}
+
+export default Ambiora
