cd108953dc2d0dca2afc52131438ef00785f346a
amalkochuparambilp <98939651+amalkochuparambilp@users.noreply.github.com>
2025-02-23 20:39:16 +0000

    prgm removed

--- src/Collageuc.jsx
+++ src/Collageuc.jsx
@@ -14,7 +14,6 @@ const Collagecu = () => {
                 <Route path="*" element={ <ErrorPage /> } />
                 <Route path="/" element={<App />} />
                 <Route path="akpdevz" element={<Dev />} />
-                <Route path="unionprogram" element={<ProgramUpdate />} />
                 <Route path="admin" element={<Admin />} />
                 <Route path="artsday" element={<Ambiora />} />
             </Routes>
