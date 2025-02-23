cd3dfc002e0794c3d022c32afae96897cc9caf92
amalkochuparambilp <98939651+amalkochuparambilp@users.noreply.github.com>
2025-02-23 21:10:13 +0000

    error

--- src/Collageuc.jsx
+++ src/Collageuc.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import { Route, Routes, Link, BrowserRouter } from "react-router-dom";
 import App from "./App";
 import Dev from "./Pages/dev";
-import ProgramUpdate from "./Pages/ProgramUpdate";
-import Admin from "./Pages/Admin";
+import Admin from "./Pages/Admin"; 
 import Ambiora from "./Pages/Ambiora";
 import ErrorPage from "./Pages/ErrorPage";
 
--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Title from "../Components/Title/Title";
 
 const ErrorPage = () => {
-    return(
-    <div style={{height: '100vh',display: 'flex',justifyContent: 'center',alignItems: 'center'}}>
-        <Title title={"404"} subTitle={"NOT FOUND"}/>
-    </div>
+    return (
+        <div>
+            <div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <Title title={"404"} subTitle={"NOT FOUND"} />
+            </div>
+        </div>
     )
 }
 
